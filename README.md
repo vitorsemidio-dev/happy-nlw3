5b83f76cb13ae061eac6be854f53828ebfe3cc90
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-10-12 10:10:11 -0300

    Readme e renomeação de pasta

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>
+    <img src=".github/logo.png"> Happy
+</h1>
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,18 @@
-console.log('server is running');
+import express from 'express';
+import 'dotenv/config';
+
+const app = express();
+
+app.use(express.json());
+
+app.get('/', (request, response) => {
+    return response.json({
+        happy: true,
+    });
+});
+
+const PORT = process.env.PORT || 3333;
+
+app.listen(PORT, () => {
+    console.log(`server is running ${PORT}`)
+});
