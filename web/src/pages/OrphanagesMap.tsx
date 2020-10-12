b1c5bfa455b28eb9b3f3d82916795e7613690b58
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-10-12 11:37:20 -0300

    Página Mapa de Orfanatos estilizada

--- web/src/pages/OrphanagesMap.tsx
+++ web/src/pages/OrphanagesMap.tsx
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { FiPlus } from 'react-icons/fi';
+
+import '../styles/pages/orphanagesMap.css';
+
+import mapMarkerImg from '../assets/img/map-marker.svg';
 
 const OrphanagesMap: React.FC = () => {
     return (
-        <div>
-            Orphanages
+        <div id="page-map">
+            <aside>
+                <header>
+                    <img src={mapMarkerImg} alt="Marcador do Mapa"/>
+
+                    <h2>Escolha um orfanato no mapa</h2>
+                    <p>Muitas crianças estão esperando a sua visita</p>
+                </header>
+
+                <footer>
+                    <strong>Rio de Janerio</strong>
+                    <span>Rio de Janeiro</span>
+                </footer>
+            </aside>
+
+            <div className="map">
+
+            </div>
+
+            <Link to="" className="create-orphanage">
+                <FiPlus size={32} color="#fff" />
+            </Link>
         </div>
     )
 }
