d8b9d0f47f830b064b5565b2991e9b100de9fed2
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-10-12 11:19:23 -0300

    Config Rotas e adição fonte

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,34 +1,13 @@
 import React from 'react';
-import { FiArrowRight } from 'react-icons/fi';
+
+import Routes from './routes';
 
 import './styles/global.css';
 import './styles/pages/landing.css';
 
-import logoImg from './assets/img/logo.svg';
-
 const App: React.FC = () => {
   return (
-    <div id="page-landing">
-      <div className="content-wrapper">
-        <img src={logoImg} alt="Logo Happy" />
-
-        <main>
-          <h1>Leve felicidade para o mundo</h1>
-          <p>
-            Visite orfanatos e mudo o dia de muitas crianças.
-          </p>
-        </main>
-
-        <div className="location">
-          <strong>Rio de Janeiro</strong>
-          <span>Rio de Janeiro</span>
-        </div>
-
-        <a href="#" className="enter-app">
-          <FiArrowRight size={26} color="rgba(0, 0, 0, 0.6)" />
-        </a>
-      </div>
-    </div>
+    <Routes />
   );
 }
 
---
+++ web/src/pages/OrphanagesMap.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrphanagesMap: React.FC = () => {
+    return (
+        <div>
+            Orphanages
+        </div>
+    )
+}
+
+export default OrphanagesMap;
