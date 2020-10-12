93fb22debbcea621298222549ec4a1a26053d259
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-10-12 10:57:40 -0300

    Estilos landing page

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
 
-function App() {
+import './styles/global.css';
+import './styles/pages/landing.css';
+
+import logoImg from './assets/img/logo.svg';
+
+const App: React.FC = () => {
   return (
-    <div>
-      Hello, Happy
+    <div id="page-landing">
+      <div className="content-wrapper">
+        <img src={logoImg} alt="Logo Happy" />
+
+        <main>
+          <h1>Leve felicidade para o mundo</h1>
+          <p>
+            Visite orfanatos e mudo o dia de muitas crianças.
+          </p>
+        </main>
+
+        <div className="location">
+          <strong>Rio de Janeiro</strong>
+          <span>Rio de Janeiro</span>
+        </div>
+
+        <a href="#" className="enter-app">
+          {'>'}
+        </a>
+      </div>
     </div>
   );
 }
