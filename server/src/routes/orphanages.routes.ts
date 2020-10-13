9cb833c63d06538bf504fd85ddbbc80a07d6bf0f
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-10-13 20:49:15 -0300

    Salvando imagens no disco

--- server/src/routes/orphanages.routes.ts
+++ server/src/routes/orphanages.routes.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
+import multer from "multer";
 
+import uploadConfig from "../config/upload";
 import OrphanagesController from "../controllers/OrphanagesController";
 
 const orphanagesRoutes = Router();
+const upload = multer(uploadConfig);
 
-orphanagesRoutes.post("/", OrphanagesController.create);
+orphanagesRoutes.post("/", upload.array("images"), OrphanagesController.create);
 orphanagesRoutes.get("/", OrphanagesController.index);
 orphanagesRoutes.get("/:id", OrphanagesController.show);
 
