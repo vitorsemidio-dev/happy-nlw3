7905ed585dac87a5f595a2c5914c02d7641fae10
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-10-13 20:32:24 -0300

    OrphanagesController (create/index/show)

---
+++ server/src/controllers/OrphanagesController.ts
@@ -0,0 +1,65 @@
+import { Request, Response } from "express";
+import { getRepository } from "typeorm";
+
+import Orphanage from "../models/Orphanage";
+
+class OrphanagesController {
+  async index(request: Request, response: Response) {
+    const orphanagesRepository = getRepository(Orphanage);
+
+    const orphanages = await orphanagesRepository.find();
+
+    return response.json(orphanages);
+  }
+  async show(request: Request, response: Response) {
+    try {
+      const { id } = request.params;
+      const orphanagesRepository = getRepository(Orphanage);
+
+      const orphanage = await orphanagesRepository.findOneOrFail(id);
+
+      return response.json(orphanage);
+    } catch (err) {
+      return response.status(404).json({
+        err: "Orphanage not found",
+      });
+    }
+  }
+
+  async create(request: Request, response: Response) {
+    try {
+      const {
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends,
+      } = request.body;
+
+      const orphanagesRepository = getRepository(Orphanage);
+
+      const orphanage = orphanagesRepository.create({
+        name,
+        latitude,
+        longitude,
+        about,
+        instructions,
+        opening_hours,
+        open_on_weekends,
+      });
+
+      await orphanagesRepository.save(orphanage);
+
+      return response.status(201).json(orphanage);
+    } catch (err) {
+      console.log(err);
+      return response.status(400).json({
+        err: "Fail to create orphanage",
+      });
+    }
+  }
+}
+
+export default new OrphanagesController();
