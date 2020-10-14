d6a13c3c89ef4e8c622bb81a2bd06efdda228166
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-10-13 21:35:43 -0300

    Handler Error

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,16 +1,18 @@
 import express from "express";
 import path from "path";
 import "dotenv/config";
+import "express-async-errors";
 import "./database/connection";
 
+import errorHandler from "./errors/handler";
 import routes from "./routes";
 
 const app = express();
 
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
-
 app.use(routes);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3333;
 
