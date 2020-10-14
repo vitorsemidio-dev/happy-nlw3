216d17b9725f71f563204ad0f54cb91b2e0a6884
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-10-13 21:54:31 -0300

    Validação com yup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import cors from "cors";
 import "dotenv/config";
 import "express-async-errors";
 import "./database/connection";
@@ -9,6 +10,7 @@ import routes from "./routes";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
 app.use(routes);
