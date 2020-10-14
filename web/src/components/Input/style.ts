05580289cef1653f9b6cde730fa51a0dda323720
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-10-14 00:11:48 -0300

    Ajustes estilo input

--- web/src/components/Input/style.ts
+++ web/src/components/Input/style.ts
@@ -1,14 +1,24 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  color: #8fa7b2;
   margin: 24px 0;
+
   label {
+    color: #8fa7b2;
     display: block;
+    margin-bottom: 8px;
   }
 
   input {
     width: 100%;
+
     border-radius: 20px;
+    height: 64px;
+    padding: 0 24px;
+    outline: none;
+
+    color: #5c8599;
+    border: 1px solid #a1e9c5;
+    background-color: #f5f8fa;
   }
 `;
--- web/src/pages/CreateOrphanage/index.tsx
+++ web/src/pages/CreateOrphanage/index.tsx
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useCallback, useState } from 'react';
+import { Map, TileLayer, Marker } from 'react-leaflet';
+import { LeafletMouseEvent } from 'leaflet';
 
 import Input from '../../components/Input';
 
@@ -6,6 +8,20 @@ import { Container, Main, Sidebar, Form } from './style';
 
 const CreateOrphanage: React.FC = () => {
   const [name, setName] = useState('');
+  const [about, setAbout] = useState('');
+  const [whatsapp, setWhatsapp] = useState('');
+  const [selectedPosition, setSelectedPosition] = useState<[number, number]>([0, 0]);
+  const [initialPosition, setInitialPosition] = useState<[number, number]>([0, 0]);
+
+  const [instructions, setInstructions] = useState('');
+  const [opening_hours, setOpeningHours] = useState('');
+  const [open_on_weekend, setOpenOnWeekend] = useState(false);
+
+  const handleMapClick = useCallback((event: LeafletMouseEvent) => {
+    const { lat, lng } = event.latlng;
+    setSelectedPosition([lat, lng]);
+  }, [setSelectedPosition]);
+
   return (
     <Container>
       <Sidebar />
@@ -13,6 +29,15 @@ const CreateOrphanage: React.FC = () => {
         <Form>
           <fieldset>
             <legend>Dados</legend>
+
+            {/* <Map center={initialPosition} zoom={15} onClick={handleMapClick}>
+              <TileLayer
+                attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+              />
+
+              <Marker position={selectedPosition} />
+            </Map> */}
 
             <Input
               name="name"
@@ -23,7 +48,56 @@ const CreateOrphanage: React.FC = () => {
               }}
             />
 
+            <Input
+              name="about"
+              label="Sobre"
+              value={about}
+              onChange={(e) => {
+                setAbout(e.target.value);
+              }}
+            />
+
+            <Input
+              name="whatsapp"
+              label="Número de Whatsapp"
+              value={whatsapp}
+              onChange={(e) => {
+                setWhatsapp(e.target.value);
+              }}
+            />
+
+            {/* TODO: Fotos */}
+
           </fieldset>
+
+          <fieldset>
+            <legend>Visitação</legend>
+
+            <Input
+              name="instructions"
+              label="Instruções"
+              value={instructions}
+              onChange={(e) => {
+                setInstructions(e.target.value);
+              }}
+            />
+
+            <Input
+              name="opening_hours"
+              label="Horário das visitas"
+              value={opening_hours}
+              onChange={(e) => {
+                setOpeningHours(e.target.value);
+              }}
+            />
+
+            {/* TODO: Atende Fim de Semana */}
+
+          </fieldset>
+
+          <button>
+            Confirmar
+          </button>
         </Form>
       </Main>
     </Container>
