0465fb26cb871f088c088cd87620c3bcfd9e167d
Emidio <52754546+vitorsemidio-dev@users.noreply.github.com>
2020-10-13 22:29:49 -0300

    Back end (#2)
    * Config database e criação primeira tabela
    
    * Primeiro registro no banco
    
    * OrphanagesController (create/index/show)
    
    * Salvando imagens no disco
    
    * Relacionamento Orphanages-Images
    
    * Views
    
    * Handler Error
    
    * Validação com yup

---
+++ server/src/routes/orphanages.routes.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import multer from "multer";
+
+import uploadConfig from "../config/upload";
+import OrphanagesController from "../controllers/OrphanagesController";
+
+const orphanagesRoutes = Router();
+const upload = multer(uploadConfig);
+
+orphanagesRoutes.post("/", upload.array("images"), OrphanagesController.create);
+orphanagesRoutes.get("/", OrphanagesController.index);
+orphanagesRoutes.get("/:id", OrphanagesController.show);
+
+export default orphanagesRoutes;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,18 +1,23 @@
-import express from 'express';
-import 'dotenv/config';
+import express from "express";
+import path from "path";
+import cors from "cors";
+import "dotenv/config";
+import "express-async-errors";
+import "./database/connection";
+
+import errorHandler from "./errors/handler";
+import routes from "./routes";
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
-
-app.get('/', (request, response) => {
-    return response.json({
-        happy: true,
-    });
-});
+app.use("/uploads", express.static(path.join(__dirname, "..", "uploads")));
+app.use(routes);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3333;
 
 app.listen(PORT, () => {
-    console.log(`server is running ${PORT}`)
+  console.log(`server is running ${PORT}`);
 });
