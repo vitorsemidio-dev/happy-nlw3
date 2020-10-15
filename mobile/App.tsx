78f6dde6cf31b01e8280cf9fad974626c4a28f57
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-10-15 13:36:23 -0300

    Estilo callout

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
-import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import MapView, { Callout, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 
 import mapMarker from './src/assets/img/map-marker.png';
 
@@ -20,11 +20,20 @@ export default function App() {
         }}>
         <Marker
           icon={mapMarker}
+          calloutAnchor={{
+            x: 2.7,
+            y: 0.8,
+          }}
           coordinate={{
             latitude: -22.9420539,
             longitude: -43.2027721,
-          }}
-        />
+          }}>
+          <Callout tooltip onPress={() => {}}>
+            <View style={styles.calloutContainer}>
+              <Text style={styles.calloutText}>Lar das meninas</Text>
+            </View>
+          </Callout>
+        </Marker>
       </MapView>
     </View>
   );
@@ -41,5 +50,19 @@ const styles = StyleSheet.create({
   map: {
     width: Dimensions.get('window').width,
     height: Dimensions.get('window').height,
+  },
+
+  calloutContainer: {
+    width: 160,
+    height: 46,
+    paddingHorizontal: 16,
+    backgroundColor: 'rgba(255, 255, 255, 0.8)',
+    borderRadius: 16,
+    justifyContent: 'center',
+  },
+
+  calloutText: {
+    color: '#0089a5',
+    fontSize: 14,
   },
 });
