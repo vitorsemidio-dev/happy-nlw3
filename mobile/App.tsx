4c903172ea57b7297b3ba133884036596f2287f0
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-10-15 13:52:17 -0300

    Footer Map

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,8 +1,15 @@
 /** @format */
 
 import React from 'react';
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native';
 import MapView, { Callout, Marker, PROVIDER_GOOGLE } from 'react-native-maps';
+import { Feather } from '@expo/vector-icons';
 
 import mapMarker from './src/assets/img/map-marker.png';
 
@@ -35,6 +42,16 @@ export default function App() {
           </Callout>
         </Marker>
       </MapView>
+
+      <View style={styles.footer}>
+        <Text style={styles.footerText}>2 orfanatos encontrados</Text>
+
+        <TouchableOpacity
+          style={styles.createOrphanageButton}
+          onPress={() => {}}>
+          <Feather name='plus' size={20} color='#fff' />
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -64,5 +81,36 @@ const styles = StyleSheet.create({
   calloutText: {
     color: '#0089a5',
     fontSize: 14,
+  },
+
+  footer: {
+    position: 'absolute',
+    left: 24,
+    right: 24,
+    bottom: 32,
+
+    backgroundColor: '#fff',
+    borderRadius: 20,
+    height: 56,
+    paddingLeft: 24,
+
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+
+    elevation: 3,
+  },
+
+  footerText: {
+    color: '#8fa7b3',
+  },
+
+  createOrphanageButton: {
+    width: 56,
+    height: 56,
+    backgroundColor: '#15c3d6',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
   },
 });
