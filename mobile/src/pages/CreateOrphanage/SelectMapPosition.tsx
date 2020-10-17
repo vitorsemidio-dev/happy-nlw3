e11fe894b500a934bdefd07f3127a81aa3d33e5c
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-10-17 16:22:07 -0300

    Copia Layout

--- mobile/src/pages/CreateOrphanage/SelectMapPosition.tsx
+++ mobile/src/pages/CreateOrphanage/SelectMapPosition.tsx
@@ -1,15 +1,71 @@
 /** @format */
 
 import React from 'react';
-import { View } from 'react-native';
-import MapView from 'react-native-maps';
+import { View, StyleSheet, Dimensions, Text } from 'react-native';
 
-const SelectMapPosition: React.FC = () => {
+import { useNavigation } from '@react-navigation/native';
+import { RectButton } from 'react-native-gesture-handler';
+import MapView, { Marker } from 'react-native-maps';
+
+import mapMarkerImg from '../../assets/img/map-marker.png';
+
+export default function SelectMapPosition() {
+  const navigation = useNavigation();
+
+  function handleNextStep() {
+    navigation.navigate('OrphanageData');
+  }
+
   return (
-    <View>
-      <MapView />
+    <View style={styles.container}>
+      <MapView
+        initialRegion={{
+          latitude: -27.2092052,
+          longitude: -49.6401092,
+          latitudeDelta: 0.008,
+          longitudeDelta: 0.008,
+        }}
+        style={styles.mapStyle}>
+        <Marker
+          icon={mapMarkerImg}
+          coordinate={{ latitude: -27.2092052, longitude: -49.6401092 }}
+        />
+      </MapView>
+
+      <RectButton style={styles.nextButton} onPress={handleNextStep}>
+        <Text style={styles.nextButtonText}>Próximo</Text>
+      </RectButton>
     </View>
   );
-};
+}
 
-export default SelectMapPosition;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    position: 'relative',
+  },
+
+  mapStyle: {
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
+  },
+
+  nextButton: {
+    backgroundColor: '#15c3d6',
+    borderRadius: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 56,
+
+    position: 'absolute',
+    left: 24,
+    right: 24,
+    bottom: 40,
+  },
+
+  nextButtonText: {
+    fontFamily: 'Nunito_800ExtraBold',
+    fontSize: 16,
+    color: '#FFF',
+  },
+});
