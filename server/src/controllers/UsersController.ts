0d2dea79d3b8b61463be15fa40463ed0d4c66268
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-10-19 17:29:38 -0300

    Migration e CRUD para Users

--- server/src/controllers/UsersController.ts
+++ server/src/controllers/UsersController.ts
@@ -1,24 +1,81 @@
 import { Request, Response } from 'express';
+import { getRepository } from 'typeorm';
+
+import User from '../models/User'
 
 class UsersController {
   public async index(request: Request, response: Response): Promise<Response> {
-    return response.json({ index: true });
+    const usersRepository = getRepository(User);
+
+    const users = await usersRepository.find();
+
+    return response.json(users);
   }
 
   public async show(request: Request, response: Response): Promise<Response> {
-    return response.json({ show: true });
+    const { id } = request.params;
+    const usersRepository = getRepository(User);
+
+    const checkUserExists = usersRepository.findOne(id);
+
+    if (!checkUserExists) {
+      return response.status(404).json({ err: 'User not found' });
+    }
+
+    return response.json(checkUserExists);
   }
 
   public async delete(request: Request, response: Response): Promise<Response> {
-    return response.json({ delete: true });
+    const { id } = request.params;
+    const usersRepository = getRepository(User);
+
+    const checkUserExist = await usersRepository.findOne(id);
+
+    if (!checkUserExist) {
+      return response.status(404).json({ err: 'User not found' });
+    }
+
+    await usersRepository.delete(checkUserExist.id);
+
+    return response.status(204).send();
   }
 
   public async create(request: Request, response: Response): Promise<Response> {
-    return response.json({ create: true });
+    const { name, email, password, passwordConfirmation } = request.body;
+    const usersRepository = getRepository(User);
+
+    const user = usersRepository.create({
+      name,
+      email,
+      password,
+    })
+
+    await usersRepository.save(user);
+
+    return response.json(user);
   }
 
   public async update(request: Request, response: Response): Promise<Response> {
-    return response.json({ update: true });
+    const usersRepository = getRepository(User);
+    const { id } = request.params;
+    const { name, email, password, passwordConfirmation } = request.body;
+
+    const checkUser = await usersRepository.findOne(id);
+
+    if (!checkUser) {
+      return response.status(404).json({ err: 'User not found' });
+    }
+
+    const userUpdated = {
+      id: checkUser.id,
+      name: name || checkUser.name,
+      email: email || checkUser.email,
+      password: password || checkUser.password,
+    }
+
+    await usersRepository.save(userUpdated);
+
+    return response.json(userUpdated);
   }
 }
 
