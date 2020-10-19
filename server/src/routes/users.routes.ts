3dd110af50640e4e340a92f9fcea36445c20d263
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-10-19 18:22:38 -0300

    users routes: validação com celebrate

--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -1,14 +1,40 @@
 import { Router } from 'express';
-import celebrate, { Joi } from 'celebrate';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import usersController from '../controllers/UsersController';
 
 const usersRoutes = Router();
 
 usersRoutes.get('/', usersController.index);
-usersRoutes.get('/:id', usersController.show);
-usersRoutes.post('/', usersController.create);
-usersRoutes.delete('/:id', usersController.delete);
-usersRoutes.put('/:id', usersController.update);
+
+usersRoutes.get('/:id', celebrate({
+  [Segments.PARAMS]: {
+    id: Joi.number().required(),
+  }
+}), usersController.show);
+
+usersRoutes.post('/', celebrate({
+  [Segments.BODY]: {
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    password: Joi.string().required(),
+    passwordConfirmation: Joi.string().required().valid(Joi.ref('password')),
+  }
+}), usersController.create);
+
+usersRoutes.delete('/:id', celebrate({
+  [Segments.PARAMS]: {
+    id: Joi.number().required(),
+  }
+}), usersController.delete);
+
+usersRoutes.put('/:id', celebrate({
+  [Segments.BODY]: {
+    name: Joi.string(),
+    email: Joi.string().email(),
+    password: Joi.string(),
+    passwordConfirmation: Joi.string().valid(Joi.ref('password')),
+  }
+}), usersController.update);
 
 export default usersRoutes;
