c658fc115603a68a5268ec1af6e8d378b806e7ba
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-10-20 14:36:38 -0300

    validação de Rota listagem de usuários

--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -1,42 +1,67 @@
-import { Router } from 'express';
-import { celebrate, Segments, Joi } from 'celebrate';
+import { Router } from "express";
+import { celebrate, Segments, Joi } from "celebrate";
 
-import usersController from '../controllers/UsersController';
-import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import usersController from "../controllers/UsersController";
+import ensureAuthenticated from "../middlewares/ensureAuthenticated";
 
 const usersRoutes = Router();
 
-usersRoutes.get('/', ensureAuthenticated, usersController.index);
+usersRoutes.get(
+  "/",
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required().token(),
+    }).unknown(),
+  }),
+  ensureAuthenticated,
+  usersController.index
+);
 
-usersRoutes.get('/:id', celebrate({
-  [Segments.PARAMS]: {
-    id: Joi.number().required(),
-  }
-}), usersController.show);
+usersRoutes.get(
+  "/:id",
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.number().required(),
+    },
+  }),
+  usersController.show
+);
 
-usersRoutes.post('/', celebrate({
-  [Segments.BODY]: {
-    name: Joi.string().required(),
-    email: Joi.string().email().required(),
-    password: Joi.string().required(),
-    passwordConfirmation: Joi.string().required().valid(Joi.ref('password')),
-  }
-}), usersController.create);
+usersRoutes.post(
+  "/",
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+      passwordConfirmation: Joi.string().required().valid(Joi.ref("password")),
+    },
+  }),
+  usersController.create
+);
 
-usersRoutes.delete('/:id', celebrate({
-  [Segments.PARAMS]: {
-    id: Joi.number().required(),
-  }
-}), usersController.delete);
+usersRoutes.delete(
+  "/:id",
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.number().required(),
+    },
+  }),
+  usersController.delete
+);
 
-usersRoutes.put('/:id', celebrate({
-  [Segments.PARAMS]: {
-    id: Joi.number().required(),
-  },
-  [Segments.BODY]: {
-    name: Joi.string(),
-    email: Joi.string().email(),
-  }
-}), usersController.update);
+usersRoutes.put(
+  "/:id",
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.number().required(),
+    },
+    [Segments.BODY]: {
+      name: Joi.string(),
+      email: Joi.string().email(),
+    },
+  }),
+  usersController.update
+);
 
 export default usersRoutes;
