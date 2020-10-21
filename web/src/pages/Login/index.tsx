8ab98bfc0d9d5d311f696f02038ebd0771300b81
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-10-21 01:39:00 -0300

    Layout login

--- web/src/pages/Login/index.tsx
+++ web/src/pages/Login/index.tsx
@@ -1,10 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+
+import Input from "../../components/Input";
 
 import logotipo from "../../assets/img/logotipo.svg";
 
-import { Container, HappyContainer, Form } from "./styles";
+import {
+  Container,
+  HappyContainer,
+  FormContainer,
+  Form,
+  FooterForm,
+} from "./styles";
 
 const Login: React.FC = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
   return (
     <Container>
       <HappyContainer>
@@ -14,7 +26,40 @@ const Login: React.FC = () => {
           <span>Rio de Janeiro</span>
         </div>
       </HappyContainer>
-      <Form>Form</Form>
+      <FormContainer>
+        <Form>
+          <fieldset>
+            <legend>Fazer login</legend>
+
+            <Input
+              name="email"
+              label="E-mail"
+              type="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+
+            <Input
+              name="password"
+              label="Senha"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </fieldset>
+
+          <FooterForm>
+            <label>
+              <input type="checkbox" />
+              Manter-me conectado
+            </label>
+
+            <button>Entrar</button>
+
+            <Link to="/forgot-password">Esqueci minha senha</Link>
+          </FooterForm>
+        </Form>
+      </FormContainer>
     </Container>
   );
 };
--- web/src/pages/Login/styles.ts
+++ web/src/pages/Login/styles.ts
@@ -31,7 +31,68 @@ export const HappyContainer = styled.div`
   }
 `;
 
-export const Form = styled.form`
-  width: 520px;
+export const FormContainer = styled.div`
+  position: relative;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 560px;
+  padding: 80px;
+
   background-color: #fff;
+`;
+
+export const Form = styled.form`
+  fieldset {
+    border: 0;
+
+    legend {
+      width: 100%;
+
+      font-size: 32px;
+      line-height: 34px;
+      color: #5c8599;
+      font-weight: 700;
+
+      margin-bottom: 16px;
+    }
+  }
+`;
+
+export const FooterForm = styled.footer`
+  color: #8fa7b2;
+  display: flex;
+  flex-direction: column;
+
+  > * + * {
+    margin-top: 24px;
+  }
+
+  input[type="checkbox"] {
+    margin-right: 16px;
+  }
+
+  button {
+    width: 100%;
+    height: 64px;
+    border: 0;
+    cursor: pointer;
+    background: #3cdc8c;
+    border-radius: 20px;
+    color: #ffffff;
+    font-weight: 800;
+
+    transition: background-color 0.2s;
+
+    &:hover {
+      background: #36cf82;
+    }
+  }
+
+  a {
+    text-align: center;
+    text-decoration: none;
+  }
 `;
