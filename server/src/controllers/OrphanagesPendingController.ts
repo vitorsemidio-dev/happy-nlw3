d265208329c199eef0dec38c594c05c11d11108f
Vitor Emidio <vitorsemidio.96@gmail.com>
2020-10-22 23:27:24 -0300

    Atualização status orfanato

--- server/src/controllers/OrphanagesPendingController.ts
+++ server/src/controllers/OrphanagesPendingController.ts
@@ -1,16 +1,27 @@
 import { Request, Response } from "express";
 import { getRepository } from "typeorm";
+import AppError from "../errors/AppError";
+
 import Orphanage from "../models/Orphanage";
 
 class OrphanagesPendingController {
   public async update(request: Request, response: Response) {
+    const { id } = request.params;
     const { approved } = request.body;
-    console.log(approved);
-    // console.log(request);
 
-    // const orphanagesRepository = getRepository(Orphanage);
+    const orphanagesRepository = getRepository(Orphanage);
 
-    return response.json();
+    const orphanage = await orphanagesRepository.findOne(id);
+
+    if (!orphanage) {
+      throw new AppError("Orphanage not found", 404);
+    }
+
+    orphanage.approved = approved;
+
+    await orphanagesRepository.save(orphanage);
+
+    return response.status(204).json();
   }
 }
 
